30a53aeef7ea601271d089678914e7e5d134b021
Pierre-Louis PAUGAM <paugam.pl@icloud.com>
2021-09-13 10:52:08 +0200

    Added assets structure (css, img...)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -21,7 +21,13 @@ class MainController extends AbstractController
      */
     public function test()
     {
-        return $this->render('main/test.html.twig');
+        $serie = [
+            "title" => "Game of Thrones",
+            "year" => 2000
+        ];
+        return $this->render('main/test.html.twig', [
+            "mySerie" => $serie,
+        ]);
     }
 
 }
